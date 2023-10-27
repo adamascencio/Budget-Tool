cc8af23660b73d9858cec623a132592f57c7b343
adamascencio <ascencioadam@gmail.com>
2023-10-26 18:22:35 -0700

    import By for DOM navigation

--- app.py
+++ app.py
@@ -3,10 +3,15 @@
 
 import os, shutil
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from utils import login_url
 from dotenv import load_dotenv
 
 load_dotenv()
 
-browser = webdriver.Chrome()
-browser.get(login_url)
+USER_EMAIL = os.getenv("EMAIL")
+USER_PASSWORD = os.getenv("PASSWORD")
+
+driver = webdriver.Chrome()
+driver.get(login_url)
+driver.find_element(By.NAME, "Email").send_keys(USER_EMAIL)
