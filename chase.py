c604c248101bfe42e79cca5114443deeeb1de57a
adamascencio <ascencioadam@gmail.com>
2023-12-06 19:12:34 -0800

    Remove chase web scraping due to bot detenction preventing login

--- chase.py
+++
@@ -1,5 +0,0 @@
-import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from utils import chase_login_url
-from dotenv import load_dotenv
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 # Store login urls and other domain specific information
 
 cap_one_login_url = "https://www.capitalone.com/"
-chase_login_url = "https://www.chase.com/"
+
